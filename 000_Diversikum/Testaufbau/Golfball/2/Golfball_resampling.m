e244b2728bc3f499dd7d84051cee6c41b7068bbe
Tobias Welti <twelti@gmail.com>
2014-12-05 15:46:37 +0100

    an der Doku gearbeitet: Glossar aufgemotzt, Abkürzungsliste

--- 000_Diversikum/Testaufbau/Golfball/2/Golfball_resampling.m
+++ 000_Diversikum/Testaufbau/Golfball/2/Golfball_resampling.m
@@ -9,3 +9,22 @@ plot(res_n(:,2));
 
 %% store
 csvwrite('golfball_10k.csv', res_n);
+
+%% cut bad edges
+res_c = res_n(100:end-100,2);
+plot(res_c);
+
+%% rescale
+clc;
+res_sc = res_c * 2047 / 1.642 -2047;
+figure(1);
+plot(res_sc(90:490));
+[m,n] = size(res_sc);
+line([0 400], [200 200], 'lineWidth',1, 'color', [1.0 0 0], 'lineStyle', '--' );
+line([0 400], [-200 -200], 'lineWidth',1, 'color', [1.0 0 0], 'lineStyle', '--' );
+line([0 400], [0 0], 'lineWidth',1, 'color', [0 1.0 0], 'lineStyle', '--' );
+
+%figure(2);
+%plot(res_sc);
+line([189 189], [1500 -1500], 'lineWidth',1, 'color', [1.0 0 1.0], 'lineStyle', '--' );
+line([219 219], [1500 -1500], 'lineWidth',1, 'color', [1.0 0 1.0], 'lineStyle', '--' );
